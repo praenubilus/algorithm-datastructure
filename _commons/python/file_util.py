7bcd4bff73c3a62e60c7119b16e9d31d21562da6
chris <praenubilus@gmail.com>
2018-10-31 20:40:14 -0400

    add and update python common utils

--- _commons/python/file_util.py
+++ _commons/python/file_util.py
@@ -2,6 +2,17 @@ import os
 import shutil
 
 
+def generate_path(*paths)->str:
+    path = os.path.join(*paths)
+    return path
+
+
+def generate_pythonic_name(fname_with_spaces: str)->str:
+    names = fname_with_spaces.strip().split(' ')
+    py_fname = '_'.join([n.lower() for n in names])
+    return py_fname
+
+
 def copy_files(src_dir, dst_dir, ext='java'):
     file_list = [f for f in os.listdir(src_dir) if f.endswith('.' + ext)]
     for file in file_list:
@@ -12,7 +23,7 @@ def copy_file(src, dst):
     shutil.copy(src, dst)
 
 
-def copy_folder(src, dst):
+def copy_dir(src, dst):
     shutil.copytree(src, dst)
 
 
@@ -23,26 +34,26 @@ def del_file(file_path):
         pass
 
 
-def del_files(from_folder, ext='class'):
-    file_list = [f for f in os.listdir(from_folder) if f.endswith('.' + ext)]
+def del_files(from_dir, ext='class'):
+    file_list = [f for f in os.listdir(from_dir) if f.endswith('.' + ext)]
     for f in file_list:
-        os.remove(os.path.join(from_folder, f))
+        os.remove(os.path.join(from_dir, f))
 
 
-def delete_folder(target_dir):
-    # clean up all files in folder
-    shutil.rmtree(target_dir, ignore_errors=True)
+def delete_dir(target):
+    # clean up all files in directory
+    shutil.rmtree(target, ignore_errors=True)
     # print('temp files cleaned up.')
 
 
-def empty_folder(target_dir):
-    for f in os.listdir(target_dir):
-        target = os.path.join(target_dir, f)
+def empty_dir(target):
+    for f in os.listdir(target):
+        target = os.path.join(target, f)
         if os.path.isdir(target):
-            delete_folder(target)
+            delete_dir(target)
         else:
             del_file(target)
 
 
-def move_folder(src, dst):
+def move_dir(src, dst):
     shutil.move(src, dst)
